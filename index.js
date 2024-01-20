bfde4d81fb0df7a759ac33b75c4f928a63db5a12
medhaholla <holla.medha@gmail.com>
2024-01-20 17:04:42 +0530

    Establish MongoDB connection in the server setup

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 require("dotenv").config();
 
@@ -8,3 +10,15 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("server started");
 });
+
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to MongoDB");
+    // Your code after successful connection
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToDatabase();
